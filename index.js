80ced9fb46730450dad97170778dc441d6e885d6
I want to be <fgelin14@gmail.com>
2017-04-15 17:46:05 +0200

    Update clear command

--- index.js
+++ index.js
@@ -50,6 +50,15 @@ client.on('message', message =>{
     }
     message.channel.bulkDelete(suppression);   
   };
+ message.channel.sendMessage("", {embed: {
+        title: "Success!",
+        color: 0x06DF00,
+        description: "Messages Suprimé!",
+        footer: {
+          text: "Message par " + name + "."
+        }
+      }});
+      }
 
  
     if(message.content.startsWith(prefix + 'lmao')){
