7cb7b6bbc3d0b93a6eb5d10f3f83ddd9faca8904
I want to be <fgelin14@gmail.com>
2017-04-15 13:22:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 const discord = require('discord.js'),
 client = new discord.Client();
-const token = "Mjk4NTQwODI1ODc1NTc4ODgw.C8_U7Q.lfxNWfL-dI2TGbiO4eQtvpo2HlQ";
+const token = "Mjk4NTQwODI1ODc1NTc4ODgw.C9OT6g.XXmUCpCGxsJMTKc2tqjH8IiCSaE";
 const prefix = "?";
 //embed_object = discord.Embed(color=my_color, title="my title")
-
+ 
 client.on('ready', () =>{
-	console.log('Patiente un peu...')
-	console.log('C\'est bon ! Je peut débuter tes commandes ! :)')
-	client.user.setGame('?help | ?add By CreepsoGaming');
+    console.log('Patiente un peu...')
+    console.log('C\'est bon ! Je peut débuter tes commandes ! :)')
+    client.user.setGame('?help | ?add By CreepsoGaming');
 });
-
+ 
 client.on('message', message =>{
-	if(message.content.startsWith(prefix + 'test')){
-		message.reply('Je fonctionne :)');
-	}
-	else if (message.content == "test"){
-		message.channel.sendMessage('Je fonctionne :)');
-	}
-});
-
-
-    if(message.content.startsWith(prefix + 'add')){
-        message.reply('Add me: **https://discordapp.com/oauth2/authorize?client_id=298540825875578880&scope=bot&permissions=2146958463**');
+    if(message.content.startsWith(prefix + 'test')){
+        message.reply('Je fonctionne :)');
     }
-    if(message.content.startsWith(prefix + 'clear')){
+    else if (message.content == "test"){
+        message.channel.sendMessage('Je fonctionne :)');
+    }else if(message.content.startsWith(prefix + 'add')){
+        message.reply('Add me: **https://discordapp.com/oauth2/authorize?client_id=298540825875578880&scope=bot&permissions=2146958463**');
+    }else if(message.content.startsWith(prefix + 'clear')){
         var chiffre = message.content.substr(6);
         let myRole = message.guild.roles.find("name", "Admin");
         let interdit = "1";
@@ -42,70 +37,63 @@ client.on('message', message =>{
    setTimeout(() => { message.delete(); }, 5000);
 });
 }
-    }
-     if (message.content == "addbot"){
+    }else if (message.content == "addbot"){
         message.channel.sendMessage('Hey, add me: **https://discordapp.com/oauth2/authorize?client_id=298540825875578880&scope=bot&permissions=2146958463**');
     }
-     });
-
-
-
+      
+ 
+ 
     if(message.content.startsWith(prefix + 'lmao')){
         message.channel.sendMessage('LMAO')
         message.channel.sendFile('lmao.jpg');
 };
-
-
+ 
+ 
     if(message.content.startsWith(prefix + 'help')){
         message.channel.sendMessage('`Check your DMs` :ok_hand:');
         message.author.sendMessage('Commands: ```lmao, ah, help, clear, add, ping, highway```');
  };
-
-
+ 
+ 
     if(message.content.startsWith(prefix + 'ah')){
         message.channel.sendMessage('AH !')
         message.channel.sendFile('AH.jpg');
 };
-
-
+ 
+ 
     if (message.content == "AH"){
         message.channel.sendFile('AH2.png')
 };
-
-
+ 
+ 
     if (message.content.startsWith(prefix + 'game')){
         client.user.setGame('?help | ?add By CreepsoGaming');
     };
-
-   if (message.content.startsWith(prefix + 'ping')){
-        message.channel.sendMessage('PONG !');
-   }else{ if (message.content == 'ping')
-        message.channel.sendMessage('PONG !');
-    };
-
+ 
+   
+ 
 //client.on('message', message =>{
     if (message.content.startsWith(prefix + 'highway')){
         message.reply('Tu devras remercier @🍪 Highway Traffic 🍪#0212 pour l\'aide qu\'il a fourni a mon créateur ! C\'est un truc de malade !')
     };
-
+ 
 //----------------------------------------------------------------------------
 if (message.content.startsWith(prefix + 'ping')){
 const now = require('performance-now');
-const helper = require('helper');
 const request = require("superagent");
-
+ 
             var debut = new Date();
-              var mentionned = msg.mentions.users.first();
+              var mentionned = message.mentions.users.first();
       var getvalueof;
       if(mentionned){
-          var getvalueof = mentionned; 
+          var getvalueof = mentionned;
       } else {
-          var getvalueof = msg.author;
+          var getvalueof = message.author;
       }
 request('http://www.google.com', function (error, response, body) {
      var fin = new Date();
     var tempsMs = fin.getTime() - debut.getTime();
-    msg.channel.sendMessage({
+    message.channel.sendMessage({
         embed: {
           type: 'rich',
           description: '',
@@ -126,5 +114,9 @@ request('http://www.google.com', function (error, response, body) {
           }
         }
 }) })
+}
+ 
 
-client.login(token);
+});
+
+client.login(token)
