27b1ec21a4e6db56b49938fdf0d71537a891be2c
I want to be <fgelin14@gmail.com>
2017-06-19 16:42:23 +0200

    Bienvenue initialisation for 2.71

---
+++ initialisation/bienvenue.js
@@ -0,0 +1,21 @@
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.find('name', 'bienvenue');
+    if(!channel) return;
+    channel.createInvite({
+            temporary : false
+        }).then(url=>{
+    channel.send("Bienvenue <@" + member.id + "> sur " + member.guild.name + '\n' + 
+    "Invite tes amis à nous rejoindre : http://discord.gg/" + url.code);
+        });
+   const channel1 = member.guild.channels.find('name', 'welcome');
+    if(!channel1) return;
+    channel1.createInvite({
+            temporary : false
+        }).then(url=>{
+    channel1.send("Bienvenue <@" + member.id + "> sur " + member.guild.name + '\n' + 
+    "Invite tes amis à nous rejoindre : http://discord.gg/" + url.code);
+     console.log(member.guild.name + ' ' + url.code);
+        });
+});
+
+module.export = bienvenue
